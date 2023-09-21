c183bbaa7dc7f7e6ce1d849f964a2a6976a1fa3f
VictorS67 <185000048@qq.com>
2023-09-21 19:54:39 -0400

    add logger

---
+++ packages/core/src/lib/logger.ts
@@ -0,0 +1,23 @@
+import { existsSync, mkdirSync } from "fs";
+import { createStream } from "rotating-file-stream";
+import { Logger } from "tslog";
+
+const logDir = "./logs";
+
+if (!existsSync(logDir)) {
+  mkdirSync(logDir);
+}
+
+const stream = createStream(`${logDir}/tslog.log`, {
+  size: "10B",
+  interval: "1d",
+  compress: "gzip",
+});
+
+const logger = new Logger();
+
+logger.attachTransport((logObj) => {
+  stream.write(JSON.stringify(logObj) + "\n");
+});
+
+export default logger;
