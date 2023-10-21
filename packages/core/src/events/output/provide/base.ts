945f66fd019af89a353677f6b1a973de7997223d
VictorS67 <185000048@qq.com>
2023-10-21 17:31:28 -0400

    update output

---
+++ packages/core/src/events/output/provide/base.ts
@@ -0,0 +1,23 @@
+import { GenerationChunk } from './generation';
+
+/**
+ * Interface that defines the source provider for providing readable
+ * source.
+ */
+export interface SourceProvider<T = unknown> {
+  source: T;
+
+  generation: GenerationChunk;
+}
+
+export abstract class BaseSourceProvider<T = unknown> implements SourceProvider<T> {
+  source: T;
+
+  generation: GenerationChunk;
+
+  abstract replace(source: T): void;
+
+  abstract add(source: T): void;
+
+  abstract provide(): GenerationChunk['output'];
+}
---
+++ packages/core/src/events/output/provide/file.ts
@@ -0,0 +1,99 @@
+import fs from 'fs';
+import { BaseSourceProvider } from './base';
+import { GenerationChunk } from './generation';
+
+export type GenerationFileChunkField = {
+  /**
+   * Generated file output.
+   * @type {string} This output represents a file path.
+   * @type {Blob} This output represetns a file in Blob.
+   */
+  output: string | Blob;
+
+  info?: Record<string, unknown>;
+};
+
+export class GenerationFileChunk extends GenerationChunk {
+  declare output: string | Blob;
+
+  constructor(fields: GenerationFileChunkField) {
+    super(fields);
+  }
+
+  protected _getOutputBlob(): Blob {
+    if (typeof this.output === 'string') {
+      return new Blob([fs.readFileSync(this.output)], {
+        type: 'application/pdf',
+      });
+    }
+
+    return this.output;
+  }
+
+  concat(chunk: GenerationFileChunk): GenerationFileChunk {
+    const newBlob = new Blob([this._getOutputBlob(), chunk._getOutputBlob()], {
+      type: 'application/pdf',
+    });
+
+    return new GenerationFileChunk({
+      output: newBlob,
+      info: {
+        ...this.info,
+        ...chunk.info,
+        source: 'blob',
+        type: newBlob.type,
+        size: newBlob.size,
+      },
+    });
+  }
+}
+
+/**
+ * Class that provide file source as a file path or a file Blob.
+ */
+export class FileProvider extends BaseSourceProvider {
+  declare source: string | Blob;
+
+  declare generation: GenerationFileChunk;
+
+  constructor(source: string | Blob) {
+    super();
+    this.source = source;
+    this.generation = new GenerationFileChunk({
+      output: source,
+      info: {
+        source: typeof source === 'string' ? source : 'blob',
+        type: typeof source === 'string' ? undefined : source.type,
+        size: typeof source === 'string' ? undefined : source.size,
+      },
+    });
+  }
+
+  replace(source: string | Blob) {
+    this.generation = new GenerationFileChunk({
+      output: source,
+      info: {
+        source: typeof source === 'string' ? source : 'blob',
+        type: typeof source === 'string' ? undefined : source.type,
+        size: typeof source === 'string' ? undefined : source.size,
+      },
+    });
+  }
+
+  add(source: string | Blob) {
+    const newGeneration = new GenerationFileChunk({
+      output: source,
+      info: {
+        source: typeof source === 'string' ? source : 'blob',
+        type: typeof source === 'string' ? undefined : source.type,
+        size: typeof source === 'string' ? undefined : source.size,
+      },
+    });
+
+    this.generation.concat(newGeneration);
+  }
+
+  provide(): string | Blob {
+    return this.generation.output;
+  }
+}
