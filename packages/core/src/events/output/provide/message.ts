5ee2edf9d36ba47648de8f89e64052e0441e396d
VictorS67 <185000048@qq.com>
2024-06-21 18:22:14 -0400

    add exports in encre-core

--- packages/core/src/events/output/provide/message.ts
+++ packages/core/src/events/output/provide/message.ts
@@ -1,7 +1,6 @@
 import {
   BaseMessage,
   type ContentLike,
-  MessageRole,
   BotMessage,
   ChatMessage,
   type ChatMessageFields,
@@ -11,65 +10,11 @@ import {
 } from '../../input/load/msgs/index.js';
 import { checkMessageRole } from '../../input/load/msgs/utils.js';
 import { type Generation, GenerationChunk } from './generation.js';
-
-/**
- * Represents the data structure of a serialized message, which includes all essential fields that describe a message.
- */
-export interface SerializedMessageData {
-  /**
-   * The main content of the message. It can be a string or a more complex structure
-   * represented by an object with key-value pairs.
-   */
-  content: ContentLike | ContentLike[];
-
-  /**
-   * The role of the message, specifying the category of the sender or purpose within the system.
-   */
-  role?: MessageRole;
-
-  /**
-   * An optional name for the message, which could be used for identifying or categorizing the message.
-   */
-  name?: string;
-
-  /**
-   * Optional additional keyword arguments that provide extended functionality and flexibility
-   * in handling the message. These might include additional data fields not covered by the standard properties.
-   */
-  additionalKwargs?: {
-    [key: string]: unknown;
-  };
-}
-
-/**
- * Represents a fully serialized message, combining the role with its detailed data.
- */
-export interface SerializedMessage {
-  /**
-   * The role of the message, defining how it should be interpreted or handled within the system.
-   */
-  role: MessageRole;
-
-  /**
-   * The detailed data of the message, structured as per SerializedMessageData.
-   */
-  json: SerializedMessageData;
-}
-
-/**
- * Defines a structure for storing serialized message data along with generated text, typically used in recording and loading.
- */
-export interface SerializedMessageGeneration {
-  /**
-   * The text content generated, often representing the processed output of the message.
-   */
-  text: string;
-
-  /**
-   * Optional serialized message, providing context or additional information related to the generated text.
-   */
-  message?: SerializedMessage;
-}
+import {
+  type SerializedMessage,
+  type SerializedMessageData,
+  type SerializedMessageGeneration,
+} from './serde.js';
 
 /**
  * Extends the basic Generation interface to specifically handle text output, commonly used in messaging systems.
