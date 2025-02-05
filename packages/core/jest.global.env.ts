5dbee5feb2c10821a41fd3ef7199cbb4870a1dd3
VictorS67 <185000048@qq.com>
2025-02-05 23:52:34 +0800

    upgrade gemini-1.0 to gemini-1.5

--- packages/core/jest.global.env.ts
+++ packages/core/jest.global.env.ts
@@ -1,3 +1,4 @@
 process.env.OPENAI_API_KEY = "you_should_get_this_api_from_openai";
 process.env.DEEPSEEK_API_KEY = "you_should_get_this_api_from_deepseek";
 process.env.MOONSHOT_API_KEY = "you_should_get_this_api_from_moonshot";
+process.env.GOOGLE_API_KEY = "you_should_get_this_api_from_google";
